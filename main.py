9e4e26411f1423ae0ac711f3f4532139f8d8813b
res_2 <res_2005@ukr.net>
2022-11-10 18:55:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pygame as pg
+import pygame.camera
+
+
+class Matrix:
+    def __init__(self, app, font_size=7):
+        self.app = app
+        self.FONT_SIZE = font_size
+        self.SIZE = self.ROWS, self.COLS = app.HEIGHT // font_size, app.WIDTH // font_size
+        self.leters = np.array(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '#', '$', '%', '&'] + ['' for i in range(10)])
+        self.font = pg.font.SysFont('ms mincho', font_size, bold=True)
+
+        self.matrix = np.random.choice(self.leters, self.SIZE)
+        self.char_intervals = np.random.randint(25, 50, size=self.SIZE)
+        self.cols_speed = np.random.randint(100, 250, size=self.SIZE)
+        self.prerendered_chars = self.get_prerendered_chars()
+
+        #self.image = self.get_image('img/WIN_20220930_22_58_54_Pro.jpg')
+
+    def get_frame(self):
+        image = app.cam.get_image()
+        image = pg.transform.scale(image, self.app.RES)
+        pixel_array = pg.pixelarray.PixelArray(image)
+        return pixel_array
+
+    def get_image(self, path_to_file):
+        image = pg.image.load(path_to_file)
+        image = pg.transform.scale(image, self.app.RES)
+        pixel_array = pg.pixelarray.PixelArray(image)
+        return pixel_array
+
+    def get_prerendered_chars(self):
+        char_colors = [(0, green, 0) for green in range(256)]
+        prerendered_chars = {}
+        for char in self.leters:
+            prerendered_char = {(char, color): self.font.render(char, True, color) for color in char_colors}
+            prerendered_chars.update(prerendered_char)
+        return prerendered_chars
+
+
+    def run(self):
+        frames = pg.time.get_ticks()
+        self.change_chars(frames)
+        self.shift_column(frames)
+        self.draw()
+
+    def shift_column(self, frames):
+        num_cols = np.argwhere(frames % self.cols_speed == 0)
+        num_cols = num_cols[:, 1]
+        num_cols = np.unique(num_cols)
+        self.matrix[:, num_cols] = np.roll(self.matrix[:, num_cols], shift=1, axis=0)
+
+
+
+    def change_chars(self, frames):
+        mask = np.argwhere(frames % self.char_intervals == 0)
+        new_chars = np.random.choice(self.leters, mask.shape[0])
+        self.matrix[mask[:, 0], mask[:, 1]] = new_chars
+
+
+    def draw(self):
+        self.image = self.get_frame()
+        for y, row in enumerate(self.matrix):
+            for x, char in enumerate(row):
+                if char:
+                    pos = x * self.FONT_SIZE, y * self.FONT_SIZE
+                    _, red, green, blue = pg.Color(self.image[pos])
+                    if red and green and blue:
+                        color = (red + green + blue) // 3
+                        color = 220 if 160 < color < 220 else color
+                        char = self.prerendered_chars[(char, (0, color, 0))]
+                        char.set_alpha(color + 60)
+                        self.app.surface.blit(char, pos)
+
+
+class MatrixVis:
+    def __init__(self):
+        self.RES = self.WIDTH, self.HEIGHT = 900, 600
+        pg.init()
+        self.screen = pg.display.set_mode(self.RES)
+        self.surface = pg.Surface(self.RES)
+        self.clock = pg.time.Clock()
+        self.matrix = Matrix(self)
+
+        pygame.camera.init()
+        self.cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+        self.cam.start()
+
+    def draw(self):
+        self.surface.fill(pg.Color('black'))
+        self.matrix.run()
+        self.screen.blit(self.surface, (0, 0))
+
+    def run(self):
+        while True:
+            self.draw()
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.flip()
+            pg.display.set_caption(str(self.clock.get_fps()))
+            self.clock.tick()
+
+if __name__ == '__main__':
+    app = MatrixVis()
+    app.run()
